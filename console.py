4d80df9ead38317a4722909862b7225b3fdbc5de
root <root@DESKTOP-CTTDNQL.localdomain>
2024-03-20 16:46:12 +0300

    end

--- console.py
+++ console.py
@@ -120,10 +120,10 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         """Create an instance of a specified class.
         Args:
-            arg (str): A string containing the class name
-            and optional attributes in the format:
-            <class_name> <attribute1=value1>
-            <attribute2=value2> ... """
+        arg (str): A string containing the class name
+        and optional attributes in the format:
+        <class_name> <attribute1=value1>
+        <attribute2=value2> ..."""
         ignored_attrs = ('id', 'created_at', 'updated_at', '__class__')
         class_name = ''
         name_pattern = r'(?P<name>(?:[a-zA-Z]|_)(?:[a-zA-Z]|\d|_)*)'
@@ -133,13 +133,28 @@ class HBNBCommand(cmd.Cmd):
             class_name = class_match.group('name')
             params_str = args[len(class_name):].strip()
             params = params_str.split(' ')
+            str_pattern = r'(?P<t_str>"([^"]|\")*")'
+            float_pattern = r'(?P<t_float>[-+]?\d+\.\d+)'
+            int_pattern = r'(?P<t_int>[-+]?\d+)'
+            param_pattern = '{}=({}|{}|{})'.format(
+                name_pattern,
+                str_pattern,
+                float_pattern,
+                int_pattern
+            )
             for param in params:
-                param_match = re.match
-                (r'({})=(.+)'.format(name_pattern), param)
+                param_match = re.fullmatch(param_pattern, param)
                 if param_match is not None:
-                    key_name = param_match.group(1)
-                    value = param_match.group(2)
-                    obj_kwargs[key_name] = value.replace('_', ' ')
+                    key_name = param_match.group('name')
+                    str_v = param_match.group('t_str')
+                    float_v = param_match.group('t_float')
+                    int_v = param_match.group('t_int')
+                    if float_v is not None:
+                        obj_kwargs[key_name] = float(float_v)
+                    if int_v is not None:
+                        obj_kwargs[key_name] = int(int_v)
+                    if str_v is not None:
+                        obj_kwargs[key_name] = str_v[1:-1].replace('_', ' ')
         else:
             class_name = args
         if not class_name:
@@ -149,6 +164,12 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
             return
         if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            if not hasattr(obj_kwargs, 'id'):
+                obj_kwargs['id'] = str(uuid.uuid4())
+            if not hasattr(obj_kwargs, 'created_at'):
+                obj_kwargs['created_at'] = str(datetime.now())
+            if not hasattr(obj_kwargs, 'updated_at'):
+                obj_kwargs['updated_at'] = str(datetime.now())
             new_instance = HBNBCommand.classes[class_name](**obj_kwargs)
             new_instance.save()
             print(new_instance.id)
