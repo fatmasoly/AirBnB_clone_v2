592003410b9ec8a7ac13f1702b737af9d24230a2
fatma <fatmacivil@hotmail.com>
2024-04-18 23:53:40 -0700

    cx

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,6 @@ def c(text):
     return "C {}".format(text)
 
 
-@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """Route function for '/python/<text>' endpoint"""
