8bad484d9a6ac9402797ed67154c4ff307ddf7da
fatma <fatmacivil@hotmail.com>
2024-04-18 23:56:27 -0700

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c(text):
     return "C {}".format(text)
 
 
-@app.route('/python/<text>', strict_slashes=False)
+@app_name.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """Route function for '/python/<text>' endpoint"""
     text = text.replace("_", " ")
