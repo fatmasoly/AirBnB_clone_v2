3c0bf0de2c69a7979b8c3c210c94bbbb623b56a2
fatma <fatmacivil@hotmail.com>
2024-04-18 23:46:11 -0700

    bbb

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""script starts a Flask web application"""
+from flask import Flask
+
+app_name = Flask(__name__)
+
+
+@app_name.route("/", strict_slashes=False)
+def home():
+    """ Route function for the home page"""
+    return "Hello HBNB!"
+
+
+@app_name.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route function for '/hbnb' endpoint"""
+    return "HBNB"
+
+
+@app_name.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Route function for '/c/<text>' endpoint"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app_name.route("/python/<text>", strict_slashes=False)
+def py(text="is cool"):
+    """Route function for '/python/<text>' endpoint"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app_name.run(host='0.0.0.0', port=5000)
