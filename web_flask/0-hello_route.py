5b0605e8fa22e1b2eb74a8e52bd23ae5d8a43bd6
fatma <fatmacivil@hotmail.com>
2024-04-18 23:08:33 -0700

    00

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script starts a Flask web application"""
+from flask import Flask
+
+app_name = Flask(__name__)
+
+@app_name.route("/", strict_slashes=False)
+def home():
+    """
+    Route function for the home page.
+
+    Returns:
+        str: A greeting message.
+    """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app_name.run(host='0.0.0.0', port=5000)
