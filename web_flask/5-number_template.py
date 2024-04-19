4f004fcd1bdd6d83e4ade2a1fc5b5b8098b43b0b
Fatma <fatmacivil@hotmail.com>
2024-04-19 10:42:53 +0300

    dd

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""script starts a Flask web application"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app_name = Flask(__name__)
+
+
+@app_name.route("/", strict_slashes=False)
+def home():
+    """ Route function for the home page"""
+    return "Hello HBNB!"
+
+
+@app_name.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Route function for '/hbnb' endpoint"""
+    return "HBNB"
+
+
+@app_name.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Route function for '/c/<text>' endpoint"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app_name.route('/python', strict_slashes=False)
+@app_name.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """Route function for '/python/<text>' endpoint"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app_name.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Route function for '/number/<n>' endpoint"""
+    return "{} is a number".format(n)
+
+
+@app_name.route('/number_template/<int:n>', strict_slashes=False)
+def page_num(n):
+    """Route function for '/number_template/<n>' endpoint"""
+    return render_template('5-number.html', n=escape(n))
+
+
+if __name__ == "__main__":
+    app_name.run(host='0.0.0.0', port=5000)
