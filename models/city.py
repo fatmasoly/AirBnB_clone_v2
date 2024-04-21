212d4884d65f0518f8a3cafae058183791c4620b
Fatma <fatmacivil@hotmail.com>
2024-04-21 08:20:46 +0300

    h

--- models/city.py
+++ models/city.py
@@ -8,12 +8,23 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """Represents a city, containing state ID and name."""
+    # __tablename__ = 'cities'
+    # if storage_type == 'db':
+    #     name = Column(String(128), nullable=False)
+    #     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    #     places = relationship('Place', backref='cities',
+    #                           cascade='all, delete, delete-orphan')
+    # else:
+    #     name = ''
+    #     state_id = ''
     __tablename__ = 'cities'
     if storage_type == 'db':
+        id = Column(String(60), primary_key=True) # Ensure this line is present
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship('Place', backref='cities',
                               cascade='all, delete, delete-orphan')
     else:
+        id = ''
         name = ''
         state_id = ''
