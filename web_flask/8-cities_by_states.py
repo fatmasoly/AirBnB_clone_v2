5724c8da8427819dd91a9b58982f2c34748856cf
fatma <fatmacivil@hotmail.com>
2024-04-20 18:46:54 -0700

    mm

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""ths is the first flask app"""
+from flask import Flask, render_template
+from models import storage
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Display HTML page: (inside the tag BODY)"""
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def close_db(exception):
+    """close db"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
