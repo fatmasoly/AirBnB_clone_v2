af6d2c6709026d17dc90f8b6495524f2aa49964e
fatma <fatmacivil@hotmail.com>
2024-04-22 19:29:42 -0700

    fx

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -2,6 +2,7 @@
 """ths is the first flask app"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ Display HTML page: (inside the tag BODY)"""
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states)
 
 
